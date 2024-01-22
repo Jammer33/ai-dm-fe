10cf363b0859547849d07b8bd98ce7fc497ba287
James Martin <j78marti@uwaterloo.ca>
2024-01-22 13:19:30 -0500

    Update socket URL to use environment variable

--- src/socket.ts
+++ src/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client';
 import Cookies from 'js-cookie';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = 'wss://localhost:3001';
+const URL = process.env.REACT_APP_SOCKET_HOST || 'wss://localhost:3001';
 
 export const socket = io(URL, {
     withCredentials: true,
