27a4393ab6eda1e184319709952cfbfe4b168831
Vansh Mago <vanshmago9@gmail.com>
2024-03-12 22:36:01 -0400

    (add) login updates

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -22,13 +22,13 @@ import { InfoOutlined } from "@mui/icons-material";
 import { useAuth } from "../../provider/AuthProvider";
 
 const LoginPage: React.FC = () => {
-<<<<<<< HEAD
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [error, setError] = React.useState("");
   const navigate = useNavigate();
   const theme = useTheme();
   const { login } = useAuth();
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const referenceButtonRef = useRef<HTMLButtonElement>(null);
   const [dynamicWidth, setDynamicWidth] = useState<number | null>(null);
@@ -78,38 +78,6 @@ const LoginPage: React.FC = () => {
       }
     } catch (error) {
       setError("Google Sign In failed");
-=======
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-    const [error, setError] = React.useState('');
-    const navigate = useNavigate();
-    const theme = useTheme();
-    const { login } = useAuth();
-    const [isLoading, setIsLoading] = React.useState(false);
-
-    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-    };
-
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    };
-
-    const handleLogin = async () => {
-        setIsLoading(true);
-        try {
-            const response = await postLogin({email, password});
-
-            if (!response.error) {
-                const username = email.split("@")[0];
-                login({email, username, userToken: response.userToken});
-                navigate('/dashboard');
-            }
-        } catch (error) {
-            setError("Invalid email or password");
-            setIsLoading(false);
-        }
->>>>>>> main
     }
   };
 
@@ -206,76 +174,10 @@ const LoginPage: React.FC = () => {
           </Link>
         </Typography>
 
-<<<<<<< HEAD
         <Spacer size={20} direction='vertical' />
       </Card>
     </Sheet>
   );
 };
-=======
-    return (
-        <Sheet 
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: '100vh',
-                width: '100vw',
-            }}
-        >
-        <Card
-            size="lg"
-            sx={{
-                width: "320px",
-                maxHeight: "70%",
-                margin: "0 20px",
-                gap: 0,
-                justifyContent: "center",
-                [theme.breakpoints.down('lg')]: {
-                    width: "280px",
-                    
-                },
-                [theme.breakpoints.down('md')]: {
-                    width: "260px",
-                    margin: "0",
-                },
-            }}
-            variant="soft"
-        >
-            {/* logo */}
-            <Logo size={175} absolute top={-0}/>
-            <Spacer size={40} direction="vertical" />
-            {/* login form */}
-            <Typography alignSelf="center" level="h2">Welcome Back</Typography>
-            <Spacer size={20} direction="vertical" />
-            <FormControl error={!!error}>
-                <FormLabel>Email</FormLabel>
-                <Input type="email" onChange={handleEmailChange} />
-            </FormControl>
-            {!!error && <Alert sx={{ backgroundColor: "background.level1", padding: "4px 0" }} size="sm" color="danger" variant="plain"><InfoOutlined /> {error}</Alert>}
-            <Spacer size={20} direction="vertical" />
-
-            <FormControl error={!!error}>
-                <FormLabel>Password</FormLabel>
-                <Input type="password" onChange={handlePasswordChange}/>
-            </FormControl>
-            <Spacer size={4} direction="vertical" />
-            <Typography level="body-sm"><Link to="/forgot-password">Forgot your password?</Link></Typography>
-            <Spacer size={40} direction="vertical" />
-
-            <Button loading={isLoading} type="submit" onClick={handleLogin}>Login</Button>
-            
-            <Spacer size={4} direction="vertical" />
-            <Typography level="body-sm">Don't have an account?<Link style={{display: "inline"}} to="/signup"> Signup</Link></Typography>
-            
-            <Spacer size={40} direction="vertical" />
-
-        </Card>
-        
-        </Sheet>
-    );
-}
->>>>>>> main
 
 export default LoginPage;
